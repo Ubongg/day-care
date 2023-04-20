41c2999a88ad04d71ba428dce4ec5a4853c117cf
Ubongg <ubongeffiong5@gmail.com>
2023-04-20 04:47:39 +0100

    initial commit'

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1M_c3kvdAkbUyedHaIscsD8_ulbzQCjI",
+  authDomain: "day-care-app-1b698.firebaseapp.com",
+  projectId: "day-care-app-1b698",
+  storageBucket: "day-care-app-1b698.appspot.com",
+  messagingSenderId: "522791364636",
+  appId: "1:522791364636:web:f4d05e6bc402e485cad7d9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
