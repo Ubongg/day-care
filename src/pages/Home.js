b4e0ac305daf299edd8d9c1ba29278db66115a71
Ubongg <ubongeffiong5@gmail.com>
2023-04-23 06:33:42 +0100

    worked on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import lf from "../assets/l&f.png";
 import hm from "../assets/hm.png";
 import et from "../assets/et.png";
 import pa from "../assets/pa.png";
+import curly from "../assets/curlylines.png";
 import "../styles/Home.css";
 
 const Home = () => {
@@ -56,6 +57,54 @@ const Home = () => {
           <h3>Play Area</h3>
           <p>Lorem ipsum dolor sit amet consectetur.</p>
         </div>
+      </div>
+      <div className="home-about">
+        <div className="home-about-col1">
+          <h6>About Us</h6>
+          <h3>Welcome To The Learning Leader Baby Care</h3>
+          <div className="home-about-col1-imgBx">
+            <img src={curly} alt="" />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+            accumsan lacus vel facilisis.
+          </p>
+        </div>
+        <div className="home-about-col2">
+          <div className="home-about-col2-list">
+            <li>Supporting the child Personality</li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor
+            </p>
+          </div>
+          <div className="home-about-col2-list">
+            <li>Professional & Qualified Teacher</li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor
+            </p>
+          </div>
+          <div className="home-about-col2-list">
+            <li>Indoor/Outdoor Games For Kids</li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor
+            </p>
+          </div>
+          <div className="home-about-col2-list">
+            <li>Best learning School For Kids</li>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor
+            </p>
+          </div>
+        </div>
+        <button>
+          <NavLink to="/login">Join Us</NavLink>
+        </button>
       </div>
     </div>
   );
